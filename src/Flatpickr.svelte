6c8a3dad3bc4ca237098b0a4163e4fbbd3dc07e0
Wiktor Kabanow <124240385+kabanowster@users.noreply.github.com>
2025-04-24 12:47:22 +0200

    Update Flatpickr.svelte

--- src/Flatpickr.svelte
+++ src/Flatpickr.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
     import flatpickr from "flatpickr";
     import {onMount} from "svelte";
+    import {addDays, compareAsc} from "date-fns";
 
-    let {options, value = $bindable(), ...attrs}: {
+    let {options, value = $bindable(), rangeDelimiter, noEndDateRender, noStartDateRender, returnAllDatesWithinRange, ...attrs}: {
         options: object,
         value?: Date | Date[] | string | null,
         class?: string,
+        style?: string,
         name?: string,
         id?: string,
         placeholder?: string,
         required?: boolean,
+        rangeDelimiter?: string,
+        noEndDateRender?: boolean,
+        noStartDateRender?: boolean,
+        returnAllDatesWithinRange?: boolean,
         onchange?: (event: Event) => void,
         onready?: (event: Event) => void,
         onopen?: (event: Event) => void,
@@ -29,6 +35,8 @@
     let fp;
     let input: Node;
     let inputValue: string | null | undefined = $state(undefined);
+
+    const defaultRangeDelimiter = " - ";
 
     const events = [
         ["ready", "onReady"],
@@ -50,7 +58,18 @@
         events.forEach(([name, hook]) => fp.config[hook].push((...detail) => input.dispatchEvent(new CustomEvent(name, {detail}))));
 
         // update input on fp date pick
-        fp.config.onChange.push((dates: Date[]) => value = dates)
+        fp.config.onChange.push((dates: Date[]) => {
+            if (dates.length === 0) {
+                value = [];
+                return;
+            }
+
+            if (dates.length === 2 && returnAllDatesWithinRange) {
+                value = getDatesBetween(dates)
+            } else {
+                value = dates;
+            }
+        })
 
         return () => fp.destroy();
     })
@@ -69,7 +88,7 @@
         // parse if value is string
         if (typeof value === "string") {
             if (value.length > 0) {
-                const parsedRange = value.split(" - ", 2);
+                const parsedRange = value.split(rangeDelimiter ?? defaultRangeDelimiter, 2);
                 if (parsedRange.length === 2) {
                     dates = [new Date(parsedRange[0]), new Date(parsedRange[1])];
                 } else {
@@ -80,7 +99,7 @@
             dates = value ? Array.isArray(value) ? value : [value] : [];
         }
 
-        fp.setDate(dates, false);
+        fp.setDate(dates.length > 0 ? [dates[0], dates[dates.length - 1]] : dates, false);
 
         switch (dates.length) {
             case 0:
@@ -90,7 +109,7 @@
                 inputValue = formatInput(dates[0]);
                 break;
             default:
-                inputValue = formatInput(dates[0]) + " - " + formatInput(dates[dates.length - 1]);
+                inputValue = (noStartDateRender ? "" : formatInput(dates[0])) + (rangeDelimiter ?? defaultRangeDelimiter) + (noEndDateRender ? "" : formatInput(dates[dates.length - 1]))
         }
 
     })
@@ -98,6 +117,34 @@
     function formatInput(date: Date): string | null {
         if (!fp || !date) return null;
         return fp.config.formatDate ? fp.config.formatDate(date) : flatpickr.formatDate(date, fp.config.dateFormat);
+    }
+
+    function getDatesBetween(dates?: { startDate: Date; endDate: Date } | Date[]): Date[] {
+        if (!dates) return [];
+        let startDate: Date;
+        let endDate: Date;
+
+        if (Array.isArray(dates)) {
+            if (dates.length !== 2) return [];
+            dates.sort((a, b) => compareAsc(a, b));
+            startDate = dates[0];
+            endDate = dates[1];
+        } else {
+            if (dates.startDate == null || dates.endDate == null || dates.endDate < dates.startDate)
+                return [];
+            startDate = dates.startDate;
+            endDate = dates.endDate;
+        }
+
+        const returnDates: Date[] = [];
+        let currentDate = new Date(startDate);
+
+        while (compareAsc(currentDate, endDate) < 1) {
+            returnDates.push(currentDate);
+            currentDate = addDays(currentDate, 1);
+        }
+
+        return returnDates;
     }
 </script>
 
